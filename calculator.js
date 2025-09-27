a5023ccad0b53f08f7bfffc1b3107eaf316f765e
remzr <110097561+remzr@users.noreply.github.com>
2025-09-27 13:53:25 +0200

    Create basic math operators

---
+++ calculator.js
@@ -0,0 +1,28 @@
+//1. Basic math operators
+
+function addNumbers(firstNumber, secondNumber) {
+    return (firstNumber + secondNumber);
+};
+
+function subtractNumbers(firstNumber, secondNumber) {
+    return (firstNumber - secondNumber);
+};
+
+function multiplyNumbers(firstNumber, secondNumber) {
+    return (firstNumber * secondNumber);
+};
+
+function divideNumbers(firstNumber, secondNumber) {
+    return (firstNumber / secondNumber);
+};
+
+//2. Variabels for User-Inputs
+
+let userNumberOne;
+let userNumberTwo;
+let userOperator;
+
+console.log(addNumbers(1,2));
+console.log(subtractNumbers(1,2));
+console.log(multiplyNumbers(1,2));
+console.log(divideNumbers(1,2));
